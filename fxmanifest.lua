fc852d21546b9c487241008a8cfb1cba36623843
Wz-FiveM <ironsxlua76@gmail.com>
2022-09-25 12:39:47 +0200

    Init project

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+---@author Wezor.
+--[[
+
+  This file is part of Wezor Project.
+
+  File [fxmanifest.lua] created at [25/09/2022 11:00]
+  Copyright (c) Wezor - All Rights Reserved
+  Unauthorized using, copying, modifying and/or distributing of this file,
+  via any medium is strictly prohibited. This code is confidential.
+
+--]]
---
+++ src/components/players/server/_Main.lua
@@ -0,0 +1,11 @@
+---@author Wezor.
+--[[
+
+  This file is part of Wezor Project.
+
+  File [_Main.lua] created at [25/09/2022 11:02]
+  Copyright (c) Wezor - All Rights Reserved
+  Unauthorized using, copying, modifying and/or distributing of this file,
+  via any medium is strictly prohibited. This code is confidential.
+
+--]]
