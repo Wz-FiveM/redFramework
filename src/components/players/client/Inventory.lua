06d3f2cab3a2a898152aa9ff8257348aed76956a
Wz-FiveM <ironsxlua76@gmail.com>
2022-09-29 10:47:22 +0200

    :package: Fix spaces

--- src/components/players/client/Inventory.lua
+++ src/components/players/client/Inventory.lua
@@ -2,4 +2,3 @@ RedFW.Shared.Event:registerEvent("receiveInventory", function(data, dataWeight)
     RedFW.Client.Components.Player.inventory.data = data.data
     RedFW.Client.Components.Player.inventory.dataWeight = dataWeight
 end)
-
