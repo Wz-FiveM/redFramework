3c6ec85a6cf9b09e282d82bf255792e929837459
Wz-FiveM <ironsxlua76@gmail.com>
2022-10-13 16:48:39 +0200

    :package: Change rage ui style and add weather and time system

---
+++ src/components/gameplay/client/_Time.lua
@@ -0,0 +1,11 @@
+---@author Wezor.
+--[[
+
+  This file is part of Wezor Project.
+
+  File [_Time.lua] created at [12/10/2022 21:03]
+  Copyright (c) Wezor - All Rights Reserved
+  Unauthorized using, copying, modifying and/or distributing of this file,
+  via any medium is strictly prohibited. This code is confidential.
+
+--]]
