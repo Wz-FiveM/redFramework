c6221f03f6b1f3d82f49dc13a55f2ccb0b4b32a2
Wz-FiveM <ironsxlua76@gmail.com>
2022-10-29 14:00:15 +0200

    :fix: Fix all bugs

---
+++ src/components/gameplay/client/_Blips.lua
@@ -0,0 +1,11 @@
+---@author Wezor.
+--[[
+
+  This file is part of Wezor Project.
+
+  File [_Blips.lua] created at [29/10/2022 11:37]
+  Copyright (c) Wezor - All Rights Reserved
+  Unauthorized using, copying, modifying and/or distributing of this file,
+  via any medium is strictly prohibited. This code is confidential.
+
+--]]
---
+++ src/components/players/server/Skin.lua
@@ -0,0 +1,11 @@
+---@author Wezor.
+--[[
+
+  This file is part of Wezor Project.
+
+  File [Skin.lua] created at [29/10/2022 11:27]
+  Copyright (c) Wezor - All Rights Reserved
+  Unauthorized using, copying, modifying and/or distributing of this file,
+  via any medium is strictly prohibited. This code is confidential.
+
+--]]
